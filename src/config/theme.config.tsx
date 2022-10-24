6e2d40b4ba03616f5abf545659cc52329c19a6d5
igor <igorlmax@yahoo.com>
2022-10-24 22:26:15 +0200

    Initialize the project, create routing, define home component

---
+++ src/config/theme.config.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createTheme } from "@mui/material";
+
+export const ThemeConfig = createTheme({
+  palette: {
+    primary: {
+      main: "#9c27b0",
+      light: "",
+      dark: "",
+    },
+    secondary: {
+      main: "#9c27b0",
+      light: "",
+      dark: "",
+    },
+  },
+});
