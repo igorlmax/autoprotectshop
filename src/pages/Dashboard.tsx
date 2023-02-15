070717df65bb775fff8eae0a77b053a2bd4d5e16
igor <igorlmax@yahoo.com>
2023-02-15 23:47:53 +0100

    Clean Dashboard page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CreateTask from "../components/tasks/CreateTask";
 import TaskList from "../components/tasks/TaskList";
-import { useDispatch } from "react-redux";
 import FilterSection from "../components/tasks/FilterSection";
 
 const Dashboard = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch({ type: "tasks/getTasks" });
-  }, []);
-
   return (
     <>
       <CreateTask />
